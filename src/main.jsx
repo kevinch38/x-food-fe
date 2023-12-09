23cf97578c4ea1e3aefd5f7a13d410ab25ee29fd
Kevin Chandra <kevinchandra38@gmail.com>
2023-12-09 13:44:30 +0700

    [SETUP] setting up necessary configurations

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import ServiceProvider from "./context/ServiceContext.jsx";
+import { RouterProvider } from 'react-router-dom';
+import setupStore from "./store.js";
+import ServiceFactory from "./services/ServiceFactory.js";
+import setupRouter from './router/router.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./index.scss";
+
+const store = setupStore();
+const router = setupRouter();
+const service = ServiceFactory();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <ServiceProvider service={service}>
+        <RouterProvider router={router} />{" "}
+      </ServiceProvider>
+    </Provider>
+  </React.StrictMode>
+);
