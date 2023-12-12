60b2ef46dcb1db0a4205b2542feba2cd8b3a4bae
asrin <asrinasinua@gmail.com>
2023-12-12 08:19:57 +0700

    [ADD]: add header and sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
     <>
       <Outlet />
+      <Sidebar/>
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,9 @@ import setupStore from "./store.js";
 import ServiceFactory from "./services/ServiceFactory.js";
 import setupRouter from './router/router.jsx';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import "./index.scss";
 
 const store = setupStore();
