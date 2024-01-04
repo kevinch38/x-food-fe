a59ca3900cb40d1cb1344bc485aa3d8c5404e46f
asrin <asrinasinua@gmail.com>
2024-01-04 09:57:07 +0700

    [PULL]: pull origin master

--- src/services/promotionService.js
+++ src/services/promotionService.js
@@ -33,7 +33,7 @@ const PromotionService = () => {
   const updatePromotion = async (promotion) => {
     const request = {
       promotionID: promotion.promotionID,
-      merchantID: promotion.merchantID,
+      merchantID: promotion.merchantName,
       promotionName: promotion.promotionName,
       maxRedeem: promotion.maxRedeem,
       promotionValue: promotion.promotionValue,
