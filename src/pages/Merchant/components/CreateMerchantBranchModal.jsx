1f20c395088eda22bdc8afb8eef3c90c6b8cd29b
Irvan-Kurniawan <irvan12fancy@gmail.com>
2024-01-10 14:13:20 +0700

    [IMPROVE] Removing Join Date

--- src/pages/Merchant/components/CreateMerchantBranchModal.jsx
+++ src/pages/Merchant/components/CreateMerchantBranchModal.jsx
@@ -67,18 +67,8 @@ export default function CreateMerchantBranchModal({
         delete data.branchID;
         dispatch(
           merchantBranchAction(async () => {
-            const now = new Date();
-            const year = now.getFullYear();
-            const month = (now.getMonth() + 1).toString().padStart(2, "0");
-            const day = now.getDate().toString().padStart(2, "0");
-            const hours = now.getHours().toString().padStart(2, "0");
-            const minutes = now.getMinutes().toString().padStart(2, "0");
-            const seconds = now.getSeconds().toString().padStart(2, "0");
-            const joinDate = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}`;
-
             await merchantBranchService.saveMerchantBranch({
-              ...data,
-              joinDate: joinDate,
+              ...data
             });
             await onGetMerchantBranches(
               merchantID,
