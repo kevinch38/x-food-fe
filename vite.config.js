d4f6cc3acf91023b270bbb6c0e0ee258e7302901
asrin <asrinasinua@gmail.com>
2024-01-18 10:36:11 +0700

    [ADD]: environment variable

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: `http://${process.env.HOST}:${process.env.PORT}`,
         changeOrigin: true,
         rewrite: (path) => path.replace('^/api/', ''),
       }
