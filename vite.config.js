6321791db3a96a5a881eae8a86958839d2736896
Irvan-Kurniawan <irvan12fancy@gmail.com>
2024-01-19 14:26:26 +0700

    [FEATURE] Deploy Ready

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
 
-dotenv.config();
+const HOST = "43.218.113.28";
+const PORT = "8081";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: `http://${process.env.HOST}:${process.env.PORT}`,
+        target: `http://${HOST}:${PORT}`,
         changeOrigin: true,
         rewrite: (path) => path.replace('^/api/', ''),
       }
