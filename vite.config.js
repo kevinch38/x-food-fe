25266aa38a9686c92dd2953a304dd24dc98a9ba1
Irvan-Kurniawan <irvan12fancy@gmail.com>
2024-01-20 21:32:27 +0700

    [IMPROVE] Improving To Be Deployed

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,34 @@
 /* eslint-disable no-undef */
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
 
 // const HOST = "43.218.113.28";
 // const PORT = "8081";
-const HOST = "localhost";
-const PORT = "8080";
+const HOST = 'localhost';
+const PORT = '8080';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: './',
-  server: {
-    proxy: {
-      '/api': {
-        target: `http://${HOST}:${PORT}`,
-        changeOrigin: true,
-        rewrite: (path) => path.replace('^/api/', ''),
-      }
-    }
-  }
-})
+	plugins: [react()],
+	build: {
+		outDir: 'build',
+		assetsDir: 'assets',
+		emptyOutDir: true,
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	base: './',
+	server: {
+		proxy: {
+			'/api': {
+				target: `http://${HOST}:${PORT}`,
+				changeOrigin: true,
+				// rewrite: (path) => path.replace('^/api/', ''),
+			},
+		},
+	},
+});
